e1766047bf3eba4230c13c37908f04911701b8b0
KhueNgocDang <khue.dn1910442@sis.hust.edu.vn>
2020-11-04 22:23:54 +0700

    Added SearchActivity and connect with SearchFragment

--- app/src/main/java/com/example/feander/SearchActivity.java
+++ app/src/main/java/com/example/feander/SearchActivity.java
@@ -1,23 +1,31 @@
 package com.example.feander;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.feander.DetailedActivity.DetailedActivity;
 import com.example.feander.Location.LocationAdapter;
 import com.example.feander.Location.LocationModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -28,7 +36,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements LocationAdapter.OnLocationListener{
 
     private RecyclerView recyclerView;
     private LocationAdapter adapter;
@@ -36,10 +44,41 @@ public class SearchActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference locationRef = db.collection("Location");
 
-    public List<LocationModel> locationList;
+    public List<LocationModel> locationList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        recyclerView = findViewById(R.id.FirestoreList);
+
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        Task<QuerySnapshot> querySnapshotTask = db.collection("Location").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
+                            LocationModel locationModel = queryDocumentSnapshot.toObject(LocationModel.class);
+                            locationList.add(locationModel);
+                        }
+                        recyclerView.setAdapter(adapter);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("TAG","onFailure" +e.getMessage());
+                    }
+                });
+        adapter = new LocationAdapter(this, locationList, this);
+    }
+
+    @Override
+    public void onLocationClick(int position) {
+        locationList.get(position);
+        Intent intent = new Intent(this, DetailedActivity.class);
+        intent.putExtra("Location",locationList.get(position));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/feander/ui/MainActivityFragment/SearchFragment.java
+++ app/src/main/java/com/example/feander/ui/MainActivityFragment/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.feander.ui.MainActivityFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.feander.MapsActivity;
 import com.example.feander.R;
+import com.example.feander.SearchActivity;
 
 public class SearchFragment extends Fragment {
 
@@ -26,7 +30,26 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        Button tea_filter_button = view.findViewById(R.id.button_tea);
+        Button shop_filter_button = view.findViewById(R.id.button_shop);
+
+        final Intent intent = new Intent(getContext(), SearchActivity.class);
+
+        tea_filter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+        shop_filter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
